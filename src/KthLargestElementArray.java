5c1f7da3183b1a5d054cee8b0b5d7cf6979be528
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-27 22:37:24 -0700

    added the comments for better catch

--- src/KthLargestElementArray.java
+++ src/KthLargestElementArray.java
@@ -1,14 +1,22 @@
 import java.util.PriorityQueue;
 
 public class KthLargestElementArray {
+    //use minimum heap, so that you can delete the smaller elements until k
+    //and then return the first element which will be the smallest among the remaining k elements
+
     static int kthLargestElementArray(int [] arr, int k){
+        //min heap
         PriorityQueue<Integer> ans = new PriorityQueue<>();
+
+        //addition in min heap
         for(int i= 0; i<arr.length; i++){
             ans.add(arr[i]);
         }
+        //keep only k elements
         while(ans.size()>k){
             ans.poll();
-        }return ans.peek();
+        }//then return first one, which will be kth largest in array, meanwhile, smallest in k elements
+        return ans.peek();
     }
 
 
