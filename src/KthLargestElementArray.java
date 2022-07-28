4bdf58ba03c889edc9cde727a8c0304f248d5581
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-27 22:34:08 -0700

    coded a method to get kth largest element in an array using min heap

--- src/KthLargestElementArray.java
+++ src/KthLargestElementArray.java
@@ -1,5 +1,16 @@
+import java.util.PriorityQueue;
+
 public class KthLargestElementArray {
-    
+    static int kthLargestElementArray(int [] arr, int k){
+        PriorityQueue<Integer> ans = new PriorityQueue<>();
+        for(int i= 0; i<arr.length; i++){
+            ans.add(arr[i]);
+        }
+        while(ans.size()>k){
+            ans.poll();
+        }return ans.peek();
+    }
+
 
 
     public static void main(String[] args) {
